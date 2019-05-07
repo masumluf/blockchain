50874dfb9c547f291ef533b92618880ff4978c7b
masum <masum.luf@gmail.com>
2019-05-07 14:31:46 +0600

    new BlockChain class created

--- index.js
+++ index.js
@@ -7,12 +7,27 @@ class Block{
         this.previoushash=previoushash;
         this.hash=this.callculateHash();
     }
-    callculateHash(){
+     callculateHash(){
         return sha256(this.timestamp+ JSON.stringify(this.data)+this.previoushash).toString();
     }
 
 }
 
+class BlockChain{
+    constructor(){
+        this.chian=[];
+    }
+
+    addChain(newBlock){
+        this.chian.push(newBlock);
+    }
+}
+
+const josscoin=new BlockChain();
+
+
 const newBlock= new Block("2019-05-07", { amount: 500 }, "ABCDEFG");
+
+josscoin.addChain(newBlock);
 
 console.log(newBlock);
