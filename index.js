3c0fc9d0b4037a0913f1e9da83fc67f6c4a2aa5e
masum <masum.luf@gmail.com>
2019-05-07 13:58:35 +0600

    hash function has been added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const sha256= require('crypto-js/sha256');
+
 class Block{
-    constructor(timestamp,data,previoushash){
+    constructor(timestamp, data ,previoushash){
         this.timestamp=timestamp;
         this.data=data;
         this.previoushash=previoushash;
+        this.hash=this.callculateHash();
+    }
+    callculateHash(){
+        return sha256(this.timestamp+ JSON.stringify(this.data)+this.previoushash);
     }
 
 }
+
+const newBlock= new Block("2019-05-07", { amount: "masum" }, "ABCDEFG");
+
+console.log(newBlock);
